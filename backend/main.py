7e9c823eeaebd927166f0a0b5b8d3adacde291ca
jlee7003 <jlee7003@naver.com>
2025-10-08 22:01:10 +0900

    20251008 Commit

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://chatbot-backend.onrender.com"],
+    allow_origins=["https://chatbot-backend-stbs.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/rag_multi_example.py
+++ backend/rag_multi_example.py
@@ -24,7 +24,7 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://chatbot-backend.onrender.com"],
+    allow_origins=["https://chatbot-backend-stbs.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
